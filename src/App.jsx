579846f4638b446e79dc69e823632268e98fa6ff
srikanth <chiramanasrikanth265@gmail.com>
2022-04-11 18:59:06 +0530

    complete todo functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Todo from './components/Todo';
+import './App.css';
+
 const App = () => {
-	return <div className='app'></div>;
+	return (
+		<div className='app'>
+			<h1>My Todo List...</h1>
+			<Todo />
+		</div>
+	);
 };
 
 export default App;
